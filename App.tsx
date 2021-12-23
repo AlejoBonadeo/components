b2e20d663ae820751ef9a0d7aadcbe769d5f35b1
Alejo Bonadeo <alejobonadeo@hotmail.com>
2021-12-23 20:31:51 -0300

    Primer Commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+
+const App = () => {
+  return (
+    <View style={{
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+      <Text style={{
+        fontSize: 20,
+        color: 'black'
+      }}>Hola Coder</Text>
+    </View>
+  )
+}
+
+export default App
